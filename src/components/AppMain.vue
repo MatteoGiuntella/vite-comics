1f1696c8a9fd344c133ed7b1f2ba5397f9161755
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-18 16:46:57 +0100

    add img

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,3 +19,6 @@ export default {
 
 <style lang="scss" scoped>
 </style>
+<!-- Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass! -->
