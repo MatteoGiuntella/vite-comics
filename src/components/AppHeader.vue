2c902770ab8b3368ce0c578cf4fb7a7497e7e0bd
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-18 18:01:36 +0100

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,22 +2,85 @@
 export default {
     data() {
         return {
-
+            item: [
+                'characters',
+                'comics',
+                'movies',
+                'tv',
+                'games',
+                'collectibles',
+                'videos',
+                'fans',
+                'news',
+                'shop',
+            ]
         };
     },
     methods: {
-
+      
     }
 }
 </script>
 
 <template>
     <header>
-       
+       <div class="container-bg">
+        <div class="container-general">
+            <div class="containerLogo">
+               <div class="logo">
+                   <img src="../../public/img/dc-logo.png" alt="">
+               </div>  
+            </div>
+            <div>
+                <nav class="navbarHeader">
+                   <ul class="navbarItem">
+                    <li  v-for="(elem, index) in item">
+                        {{ elem }}
+                    </li>
+                   </ul>
+                </nav>
+            </div>
+        </div>
+       </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
+header {
+    width: 1000px;
+    .container-bg{
+        width: 100%;
+        background-color: white;
+        .container-general {
+            height: 100px;
+            width: 70%;
+            display: flex;
+            flex-wrap: nowrap;
+            justify-content: space-between;
+            margin-left: 200px;
+            .containerLogo{
+                width: 30%;
+                height: 100%;
+                .logo{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+            .navbarHeader{
+                width: 50%;
+                height: 100%;
+                .navbarItem{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    flex-wrap: nowrap;
+                }
+            }
+
+        }
+    }
+
+}
 </style>
 <!-- Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
 Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
